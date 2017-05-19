724c4d751a0d47d4ce0c629a96a2b44be3b31d6b
Michel Kansou <michel.kansou@outlook.fr>
2017-05-19 12:09:08 +0000

    add detection tests

---
+++ tests/detect_object.py
@@ -0,0 +1,78 @@
+# import the necessary packages
+from picamera.array import PiRGBArray
+from picamera import PiCamera
+from collections import deque
+import numpy as np
+import argparse
+import imutils
+import time
+import cv2
+
+
+def isset(v):
+    try:
+        type(eval(v))
+    except BaseException:
+        return 0
+    else:
+        return 1
+
+
+# define the lower and upper boundaries of the coke
+# define the list of boundaries
+lower = (120, 80, 80)
+upper = (180, 255, 255)
+
+
+# initialize the camera and grab a reference to the raw camera capture
+camera = PiCamera()
+camera.resolution = (640, 480)
+camera.framerate = 32
+rawCapture = PiRGBArray(camera, size=(640, 480))
+
+# allow the camera to warmup
+time.sleep(0.1)
+
+# capture frames from the camera
+for frame in camera.capture_continuous(
+        rawCapture, format="bgr", use_video_port=True):
+        # grab the raw NumPy array representing the image, then initialize the timestamp
+        # and occupied/unoccupied text
+    image = frame.array
+    image = imutils.resize(image, width=600)
+    blurred = cv2.GaussianBlur(image, (11, 11), 0)
+    hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
+
+    mask = cv2.inRange(hsv, lower, upper)
+    mask2 = mask.copy()
+
+    # find contours in the mask image
+    contours, hierarchy = cv2.findContours(
+        mask2, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+
+    # finding contour with maximum area and store it as best_cnt
+    max_area = 0
+    for cnt in contours:
+        area = cv2.contourArea(cnt)
+        if area > max_area:
+            max_area = area
+            best_cnt = cnt
+
+    # finding centroids of best_cnt and draw a circle there
+    if isset('best_cnt'):
+        M = cv2.moments(best_cnt)
+        cx, cy = int(M['m10'] / M['m00']), int(M['m01'] / M['m00'])
+        cv2.circle(frame, (cx, cy), 5, 255, -1)
+        print("Central pos: (%d, %d)" % (cx, cy))
+    else:
+        print("[Warning]Tag lost...")
+    # show the frame
+    cv2.imshow("Frame", image)
+    key = cv2.waitKey(1) & 0xFF
+
+    # clear the stream in preparation for the next frame
+    rawCapture.truncate(0)
+
+    # if the `q` key was pressed, break from the loop
+    if key == ord("q"):
+        break
