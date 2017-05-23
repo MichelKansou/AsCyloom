43183715f01ae40df613fdf2115f7101fa05c32d
Michel Kansou <michel.kansou@outlook.fr>
2017-05-23 10:33:39 +0200

    update can detection

--- can_detection.py
+++ can_detection.py
@@ -2,6 +2,7 @@
 from picamera.array import PiRGBArray
 from picamera import PiCamera
 import time
+import smbus
 import cv2
 
 def isset(v):
@@ -16,11 +17,21 @@ def isset(v):
 cascPath = './resources/cascade_5.xml'
 cokeCascade = cv2.CascadeClassifier(cascPath)
 
+# I2C Raspberry
+bus = smbus.SMBus(1)
+address = 0x12
+
 # initialize the camera and grab a reference to the raw camera capture
 camera = PiCamera()
 camera.resolution = (640, 480)
 camera.framerate = 32
 rawCapture = PiRGBArray(camera, size=(640, 480))
+
+
+# initialize IA
+searching = True
+goToBase = False
+
 
 # allow the camera to warmup
 time.sleep(0.1)
@@ -31,29 +42,36 @@ for frame in camera.capture_continuous(
         # grab the raw NumPy array representing the image, then initialize the timestamp
         # and occupied/unoccupied text
     image = frame.array
-
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cokes = cokeCascade.detectMultiScale(gray, 2, 25)
-
-    # finding centroids of best_cnt and draw a circle there
-    if isset('cokes'):
-        # Draw a rectangle around the faces
-        for (x, y, w, h) in cokes:
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            bx = x + w
-            by = y + h
-            ox = (x + bx)/2
-            oy = (y + by)/2
-            print("A :(%d, %d)" % (x, y))
-            if (ox > 280 and ox < 360):
-               print("Center")
-            if ox > 360: 
-               print("Right")
-            if ox < 280:
-               print("Left")
-            print("Central pos: (%d, %d)" % (ox, oy))
+    if searching:
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cokes = cokeCascade.detectMultiScale(gray, 2, 25)
+        # If a can of coke was found
+        if isset('cokes'):
+            # Draw a rectangle around the cokes
+            for (x, y, w, h) in cokes:
+                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                bx = x + w
+                by = y + h
+                ox = (x + bx)/2
+                oy = (y + by)/2
+                print("A :(%d, %d)" % (x, y))
+                if (ox > 280 and ox < 360):
+                    print("Center")
+                    print ("Send Move Forward")
+                    bus.write_byte(address, 1)
+                if ox > 360:
+                    print("Right")
+                    print("Send Move to Right")
+                    bus.write_byte(address, 4)
+                if ox < 280:
+                    print("Left")
+                    print("Send Move to Left")
+                    bus.write_byte(address, 3)
+                print("Central pos: (%d, %d)" % (ox, oy))
+    if goToBase:
+        print("Going to base sir")
     else:
-        print("[Warning]Tag lost...")
+        print("[Warning]Target lost...")
 
     # show the frame
     cv2.imshow("Tracking", image)
