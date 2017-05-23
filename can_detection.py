351b51f467027ec0e99a84952d41457a68f82c98
Michel Kansou <michel.kansou@outlook.fr>
2017-05-23 18:33:31 +0200

    update extraction base

--- can_detection.py
+++ can_detection.py
@@ -30,8 +30,14 @@ rawCapture = PiRGBArray(camera, size=(640, 480))
 
 # define the lower and upper boundaries for extraction point
 # define the list of boundaries
-lower = (17, 15, 100)
-upper = (50, 56, 200)
+# lower = (17, 15, 100)
+# upper = (50, 56, 200)
+boundaries = [
+	([17, 15, 100], [50, 56, 200]),
+	([86, 31, 4], [220, 88, 50]),
+	([25, 146, 190], [62, 174, 250]),
+	([103, 86, 65], [145, 133, 128])
+]
 
 
 # initialize IA
@@ -102,10 +108,15 @@ for frame in camera.capture_continuous(
         blurred = cv2.GaussianBlur(image, (11, 11), 0)
         hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-        mask = cv2.inRange(hsv, lower, upper)
-        mask = cv2.erode(mask, None, iterations=2)
-        mask = cv2.dilate(mask, None, iterations=2)
-        ColorDetectionImage = mask.copy()
+        # loop over the boundaries
+        for (lower, upper) in boundaries:
+        	# create NumPy arrays from the boundaries
+        	lower = np.array(lower, dtype = "uint8")
+        	upper = np.array(upper, dtype = "uint8")
+            mask = cv2.inRange(hsv, lower, upper)
+            mask = cv2.erode(mask, None, iterations=2)
+            mask = cv2.dilate(mask, None, iterations=2)
+            ColorDetectionImage = mask.copy()
 
         # find contours in the mask image
         contours = cv2.findContours(ColorDetectionImage, cv2.RETR_EXTERNAL,
