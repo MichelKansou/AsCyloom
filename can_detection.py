ab365ae6db34a3c027566892d39ce70d42c81a23
Michel Kansou <michel.kansou@outlook.fr>
2017-05-23 15:24:59 +0200

    update can detection

--- can_detection.py
+++ can_detection.py
@@ -5,14 +5,6 @@ import time
 import smbus
 import cv2
 
-def isset(v):
-    try:
-        type(eval(v))
-    except BaseException:
-        return 0
-    else:
-        return 1
-
 # cascade directory
 cascPath = './resources/cascade_5.xml'
 cokeCascade = cv2.CascadeClassifier(cascPath)
@@ -44,55 +36,54 @@ for frame in camera.capture_continuous(
         # grab the raw NumPy array representing the image, then initialize the timestamp
         # and occupied/unoccupied text
     image = frame.array
-    #if (searching == True and goToBase == False):
-    gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-    cokes = cokeCascade.detectMultiScale(gray, 2, 25)
-    # If a can of coke was found
-    print (isset('cokes'))
-    if isset('cokes'):
-        print("Object Detected")
-        #if objectDetected != 1:
-           # objectDetected = 1
-           # bus.write_byte(address, 11)
-        # Draw a rectangle around the cokes
-        for (x, y, w, h) in cokes:
-            cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
-            bx = x + w
-            by = y + h
-            ox = (x + bx)/2
-            oy = (y + by)/2
-            print("Target  position: (%d, %d)" % (ox, oy))
-    else:
-        print("[Warning]Target lost...")
-        #targetLost += 1
-        #if objectDetected != 0:
-            #objectDetected = 0
-            #bus.write_byte(address, 12)
-            #    if (ox > 280 and ox < 360):
-            #        print("Center")
-            #        print ("Send Move Forward")
-            #        if direction != 1:
-            #            direction = 1
-                        #bus.write_byte(address, 1)
-            #    elif ox > 360:
-            #        print("Right")
-            #        print("Send Move to Right")
-            #        if direction != 4:
-            #            direction = 4
-            #            bus.write_byte(address, 4)
-            #    elif ox < 280:
-            #        print("Left")
-            #        print("Send Move to Left")
-            #        if direction !=3:
-            #            direction = 3
-            #            bus.write_byte(address, 3)
-#    if (goToBase == True and searching == False):
-#        targetLost = 0
-#        print("Going to base sir")
-#    if (targetLost > 20 and direction != 0):
-#        print("Stop")
-#        direction = 0
-#        bus.write_byte(address, 0)
+    if (searching == True and goToBase == False):
+        gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+        cokes = cokeCascade.detectMultiScale(gray, 2, 25)
+        # If a can of coke was found
+        if len('cokes') > 0:
+            if objectDetected != 1:
+                 objectDetected = 1
+                 bus.write_byte(address, 11)
+            # Draw a rectangle around the cokes
+            for (x, y, w, h) in cokes:
+                cv2.rectangle(image, (x, y), (x + w, y + h), (0, 255, 0), 2)
+                bx = x + w
+                by = y + h
+                ox = (x + bx)/2
+                oy = (y + by)/2
+                print("A :(%d, %d)" % (x, y))
+                if (ox > 280 and ox < 360):
+                   print("Center")
+                   print ("Send Move Forward")
+                   if direction != 1:
+                       direction = 1
+                        bus.write_byte(address, 1)
+                elif ox > 360:
+                   print("Right")
+                   print("Send Move to Right")
+                   if direction != 4:
+                       direction = 4
+                       bus.write_byte(address, 4)
+                elif ox < 280:
+                   print("Left")
+                   print("Send Move to Left")
+                   if direction !=3:
+                       direction = 3
+                       bus.write_byte(address, 3)
+                print("Target  position: (%d, %d)" % (ox, oy))
+        else:
+           targetLost += 1
+           if objectDetected != 0:
+              objectDetected = 0
+              bus.write_byte(address, 12)
+           print("[Warning]Target lost...")
+        if (goToBase == True and searching == False):
+            targetLost = 0
+            print("Going to base sir")
+        if (targetLost > 20 and direction != 0):
+            print("Stop")
+            direction = 0
+            bus.write_byte(address, 0)
     # show the frame
     cv2.imshow("Tracking", image)
     key = cv2.waitKey(1) & 0xFF
